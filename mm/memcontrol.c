717ce2638cabe8cd3a56e0e0872798c7ea7cdcb7
Martijn Coenen <maco@google.com>
2016-01-14 14:33:52 +0100

    UPSTREAM: memcg: Only free spare array when readers are done
    A spare array holding mem cgroup threshold events is kept around
    to make sure we can always safely deregister an event and have an
    array to store the new set of events in.
    
    In the scenario where we're going from 1 to 0 registered events, the
    pointer to the primary array containing 1 event is copied to the spare
    slot, and then the spare slot is freed because no events are left.
    However, it is freed before calling synchronize_rcu(), which means
    readers may still be accessing threshold->primary after it is freed.
    
    Fixed by only freeing after synchronize_rcu().
    
    Change-Id: Id324c5c15cc6aaa684c6e84c1049f723c6c6d984
    Signed-off-by: Martijn Coenen <maco@google.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: MassStash <MassStash@gmail.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3539,16 +3539,17 @@ static void mem_cgroup_usage_unregister_event(struct cgroup *cgrp,
 swap_buffers:
 	
 	thresholds->spare = thresholds->primary;
-	
-	if (!new) {
-		kfree(thresholds->spare);
-		thresholds->spare = NULL;
-	}
 
 	rcu_assign_pointer(thresholds->primary, new);
 
 	
 	synchronize_rcu();
+
+	/* If all events are unregistered, free the spare array */
+	if (!new) {
+		kfree(thresholds->spare);
+		thresholds->spare = NULL;
+	}
 unlock:
 	mutex_unlock(&memcg->thresholds_lock);
 }
